93139989ee3224ae90e9ba1e3e263fd5ed3507cb
Amy Holt <amy.elizabeth.holt@gmail.com>
2022-01-25 20:07:02 -0700

    Add Hash iteration activity

---
+++ hashes_2.rb
@@ -0,0 +1,45 @@
+# 1- Using the following Hash
+
+states = {
+  "Oregon" => "OR",
+  "Alabama" => "AL",
+  "New Jersey" => "NJ",
+  "Colorado" => "CO"
+}
+
+# Use the each method to iterate through it and print out a sentence stating each state name and abbreviation
+#
+#
+#
+
+
+# ======================================
+# 2- Using the following Hash
+
+birthdays = {
+  "Cindy" => "June 10",
+  "Rigo" => "March 31",
+  "Amos" => "July 21",
+  "Jeff" => "February 12"
+}
+
+# Use the each method to iterate through it and print out a sentence stating each friends name and birthday
+#
+#
+#
+
+
+# ======================================
+# 3- Using the following Hash
+
+login_statuses = {
+  "Cindy" => true,
+  "Rigo" => false,
+  "Amos" => true,
+  "Jeff" => true
+}
+
+# Use the each method to iterate through it and print ONLY the names of friends who are "logged in"
+#
+#
+#
