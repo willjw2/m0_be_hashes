a50797ca6da2544c50b170ae59e9979c6fde95d7
Amy Holt <amy.elizabeth.holt@gmail.com>
2022-02-17 10:58:09 -0700

    Update hashes_1.rb

--- hashes_1.rb
+++ hashes_1.rb
@@ -6,27 +6,22 @@ Activity:
 # file by entering the following command in your Terminal: 
 # `ruby hashes_1.rb`
 
-# Example: Write code that prints a hash holding grocery store inventory:
-foods = {apples: 23, grapes: 507, eggs: 48}
-p foods
-
-# Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+foods = {"apples": 23, "grapes": 507, "eggs": 48}
 p zoo
 
-# Write code that prints all of the 'keys' of the zoo variable 
+# Write code that prints all of the 'keys' of the foods variable 
 # you created above:
 # YOUR CODE HERE
 
-# Write code that prints all of the 'values' of the zoo variable 
+# Write code that prints all of the 'values' of the foods variable 
 # you created above:
 # YOUR CODE HERE
 
-# Write code that prints the value of the first animal of the zoo variable 
+# Write code that prints the value of the second food of the foods variable 
 # you created above:
 # YOUR CODE HERE
 
-# Write code that adds an animal to the zoo hash. 
+# Write code that adds a food to the foods hash. 
 # Then, print the updated hash:
 # YOUR CODE HERE
 
